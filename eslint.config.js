30d871f114a857e1825c6b6465b77a70757a85e1
bot19 <jsbot@pm.me>
2025-07-26 19:35:04 +1000

    add eslint a11y plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
-import prettier from 'eslint-config-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import reactX from 'eslint-plugin-react-x';
+import reactDom from 'eslint-plugin-react-dom';
+import prettier from 'eslint-config-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    ...jsxA11y.flatConfigs.recommended,
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
@@ -24,12 +26,15 @@ export default tseslint.config([
       prettier,
     ],
     languageOptions: {
+      ...jsxA11y.flatConfigs.recommended.languageOptions,
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
         project: ['./tsconfig.node.json', './tsconfig.app.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
-])
+]);
