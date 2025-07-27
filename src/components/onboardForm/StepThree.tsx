e23eb69d17182ee8cc83e919250cc76d45582c13
bot19 <jsbot@pm.me>
2025-07-27 22:00:54 +1000

    step3 UI

--- src/components/onboardForm/StepThree.tsx
+++ src/components/onboardForm/StepThree.tsx
@@ -1,169 +1,73 @@
-import { ChevronDownIcon } from '@heroicons/react/16/solid';
-
-const days = Array.from({ length: 31 }, (_, i) => i + 1);
-const months = Array.from({ length: 12 }, (_, i) => i + 1);
-const years = Array.from({ length: 100 }, (_, i) => 2025 - i);
+import { EyeIcon } from '@heroicons/react/16/solid';
+import { Feedback, ProgressBar } from '../../ui';
 
 export const StepThree = () => {
   return (
     <form action="#" method="POST" className="space-y-6">
+      <Feedback />
+
       <div>
-        <div className="flex justify-between">
-          <label
-            htmlFor="fullName"
-            className="block text-sm/6 font-medium text-gray-900"
+        <label
+          htmlFor="password"
+          className="block text-sm/6 font-medium text-gray-900"
+        >
+          Create your password
+        </label>
+        <div className="mt-2 flex">
+          <div className="-mr-px flex-1">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              autoComplete="current-password"
+              className="block w-full rounded-l-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6"
+            />
+          </div>
+          <button
+            type="button"
+            className="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-primary cursor-pointer"
           >
-            Full name
-          </label>
-          <span id="fullName-required" className="text-sm/6 text-gray-500">
-            Required
-          </span>
-        </div>
-        <div className="mt-2">
-          <input
-            id="fullName"
-            name="fullName"
-            type="text"
-            maxLength={100}
-            placeholder="Enter full name"
-            aria-describedby="fullName-required"
-            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary xs:text-sm/6"
-          />
+            <EyeIcon
+              aria-hidden="true"
+              className="-ml-0.5 size-4 text-gray-400"
+            />
+            Show
+          </button>
         </div>
       </div>
 
+      <ProgressBar />
+
       <div>
-        <div className="flex justify-between">
-          <label
-            htmlFor="email"
-            className="block text-sm/6 font-medium text-gray-900"
+        <label
+          htmlFor="confirm-password"
+          className="block text-sm/6 font-medium text-gray-900"
+        >
+          Confirm your password
+        </label>
+        <div className="mt-2 flex">
+          <div className="-mr-px flex-1">
+            <input
+              id="confirm-password"
+              name="confirm-password"
+              type="password"
+              required
+              autoComplete="current-password"
+              className="block w-full rounded-l-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6"
+            />
+          </div>
+          <button
+            type="button"
+            className="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-primary cursor-pointer"
           >
-            Email
-          </label>
-          <span id="email-required" className="text-sm/6 text-gray-500">
-            Required
-          </span>
+            <EyeIcon
+              aria-hidden="true"
+              className="-ml-0.5 size-4 text-gray-400"
+            />
+            Show
+          </button>
         </div>
-        <div className="mt-2">
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="you@example.com"
-            aria-describedby="email-required"
-            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary xs:text-sm/6"
-          />
-        </div>
-      </div>
-
-      <div>
-        <fieldset>
-          <div className="flex justify-between">
-            <legend className="block text-sm/6 font-medium text-gray-900">
-              Date of birth
-            </legend>
-            <span id="dob-required" className="text-sm/6 text-gray-500">
-              Required
-            </span>
-          </div>
-
-          <div className="mt-2 grid grid-cols-3">
-            <div className="grid grid-cols-1 focus-within:relative">
-              <select
-                id="day"
-                name="day"
-                autoComplete="day"
-                aria-label="Day"
-                className="col-start-1 row-start-1 w-full appearance-none rounded-l-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary xs:text-sm/6"
-              >
-                <option value="">Day</option>
-                {days.map(day => (
-                  <option key={day} value={day}>
-                    {day}
-                  </option>
-                ))}
-              </select>
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 xs:size-4"
-              />
-            </div>
-
-            <div className="grid grid-cols-1 focus-within:relative -mx-px">
-              <select
-                id="month"
-                name="month"
-                autoComplete="month"
-                aria-label="Month"
-                className="col-start-1 row-start-1 w-full appearance-none bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary xs:text-sm/6"
-              >
-                <option value="">Month</option>
-                {months.map(month => (
-                  <option key={month} value={month}>
-                    {month}
-                  </option>
-                ))}
-              </select>
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 xs:size-4"
-              />
-            </div>
-
-            <div className="grid grid-cols-1 focus-within:relative">
-              <select
-                id="year"
-                name="year"
-                autoComplete="year"
-                aria-label="Year"
-                className="col-start-1 row-start-1 w-full appearance-none rounded-r-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary xs:text-sm/6"
-              >
-                <option value="">Year</option>
-                {years.map(year => (
-                  <option key={year} value={year}>
-                    {year}
-                  </option>
-                ))}
-              </select>
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 xs:size-4"
-              />
-            </div>
-          </div>
-        </fieldset>
-      </div>
-
-      <div>
-        <fieldset>
-          <label
-            htmlFor="gender"
-            className="block text-sm/6 font-medium text-gray-900"
-          >
-            Gender
-          </label>
-
-          <div className="mt-2">
-            <div className="grid grid-cols-1 focus-within:relative">
-              <select
-                id="gender"
-                name="gender"
-                autoComplete="gender"
-                aria-label="Gender"
-                className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary xs:text-sm/6"
-              >
-                <option value="">Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-              </select>
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 xs:size-4"
-              />
-            </div>
-          </div>
-        </fieldset>
       </div>
 
       <div className="grid grid-cols-4 gap-4">
@@ -179,7 +83,7 @@ export const StepThree = () => {
             type="submit"
             className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary cursor-pointer"
           >
-            Next
+            Submit
           </button>
         </div>
       </div>
