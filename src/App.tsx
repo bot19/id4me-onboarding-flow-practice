5d0c6602974be01f9d634f99f5e1521a8ff18047
bot19 <jsbot@pm.me>
2025-07-27 17:18:32 +1000

    use Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { OnboardForm } from './components/onboardForm';
+import { Logo } from './ui';
 
 function App() {
   return (
-    <>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <Logo />
       <OnboardForm />
-    </>
+    </div>
   );
 }
 
