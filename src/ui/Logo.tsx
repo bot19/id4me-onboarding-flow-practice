967ed4e8ea2e2207cc928be2b781e276fea44c48
bot19 <jsbot@pm.me>
2025-07-27 17:17:54 +1000

    abstract Logo component

---
+++ src/ui/Logo.tsx
@@ -0,0 +1,12 @@
+// TODO: Replace with actual logo
+export const Logo = () => {
+  return (
+    <div className="sm:mx-auto sm:w-full sm:max-w-md">
+      <img
+        alt="Your Company"
+        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+        className="mx-auto h-10 w-auto"
+      />
+    </div>
+  );
+};
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,1 +1,2 @@
+export { Logo } from './Logo';
 export { ProgressIndicator } from './ProgressIndicator';
