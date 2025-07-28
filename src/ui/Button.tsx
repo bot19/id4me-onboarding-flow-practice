44c4cd9a0cb7afa2e24898a3f44e69080b89b634
bot19 <jsbot@pm.me>
2025-07-29 08:10:45 +1000

    button component  colours

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,9 +1,11 @@
 import { cn } from '../utils';
 
 interface ButtonProps {
+  colour: 'primary' | 'light';
   disabled?: boolean;
   text: string;
   type: 'submit' | 'button';
+  onClick?: () => void;
 }
 
 export const Button = (props: ButtonProps) => {
@@ -11,10 +13,14 @@ export const Button = (props: ButtonProps) => {
     <button
       type={props.type}
       className={cn(
-        'flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary cursor-pointer',
+        'flex w-full justify-center rounded-md  px-3 py-1.5 text-sm/6 font-semibold shadow-xs  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary cursor-pointer',
+        props.colour === 'primary' && 'bg-primary text-white hover:bg-primary',
+        props.colour === 'light' &&
+          'bg-white text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50',
         props.disabled && 'disabled:cursor-not-allowed disabled:opacity-50'
       )}
       disabled={props.disabled}
+      onClick={props.onClick}
     >
       {props.text}
     </button>
