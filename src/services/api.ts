b1353645d5e7baf86bf37dacf5d754fbb650a68d
bot19 <jsbot@pm.me>
2025-07-29 07:44:56 +1000

    clean up api type + mock auth token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,31 +1,29 @@
 export const API_URL_SEND_OTP = '/auth/sendOTP';
 export const API_URL_VALIDATE_OTP = '/auth/validateOTP';
 
-// const FIFTEEN_MINS_AS_MS = 15 * 60 * 1000;
+const FIFTEEN_MINS_AS_MS = 15 * 60 * 1000; // 15 mins
 
-interface ApiResponse<T = unknown> {
+interface ApiResponse {
   success: boolean;
-  data?: T;
+  response?: string;
   error?: string;
   until?: string;
 }
 
 class MockApiService {
-  async post<T = unknown>(
-    url: string,
-    payload: unknown
-  ): Promise<ApiResponse<T>> {
+  async post(url: string, payload: string): Promise<ApiResponse> {
     await new Promise(resolve => setTimeout(resolve, 500));
 
     switch (url) {
       case API_URL_SEND_OTP: {
-        const { mobile } = payload as { mobile?: string };
+        // need to interigate mobile num. to simulate response types
+        const { mobile } = JSON.parse(payload) as { mobile?: string };
 
         if (mobile === '0433222111') {
           return {
             success: false,
             error: 'Mobile already in use',
-          } as ApiResponse<T>;
+          } as ApiResponse;
         }
 
         // simulate network/server error
@@ -33,27 +31,26 @@ class MockApiService {
           return {
             success: false,
             error: 'Issue encountered, please try again later',
-          } as ApiResponse<T>;
+          } as ApiResponse;
         }
 
         return {
           success: true,
-          data: 'OTP sent successfully' as T,
-        } as ApiResponse<T>;
+          response: '{ "data": "OTP sent successfully" }',
+        } as ApiResponse;
       }
 
       case API_URL_VALIDATE_OTP: {
-        const { otp } = payload as { otp?: string };
+        const { otp } = JSON.parse(payload) as { otp?: string };
 
         if (otp === '000000') {
-          return { success: false, error: 'Wrong OTP code' } as ApiResponse<T>;
+          return { success: false, error: 'Wrong OTP code' } as ApiResponse;
         }
 
         return {
           success: true,
-          data: 'OTP validated successfully' as T,
-          // until: `${Date.now() + FIFTEEN_MINS_AS_MS}`,
-        } as ApiResponse<T>;
+          response: `{ "data": "OTP validated successfully", "expires": ${Date.now() + FIFTEEN_MINS_AS_MS}, "bearer": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." }`,
+        } as ApiResponse;
       }
 
       default:
