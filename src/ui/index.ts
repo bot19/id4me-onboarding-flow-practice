92d5e43fef193d15a904bcb68157dee1134172ff
bot19 <jsbot@pm.me>
2025-07-29 11:36:13 +1000

    InputSelect + Heading component

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,6 +1,8 @@
 export { Button } from './Button';
 export { Feedback } from './Feedback';
+export { Heading } from './Heading';
 export { InputBasic } from './InputBasic';
+export { InputSelect } from './InputSelect';
 export { Logo } from './Logo';
 export { ProgressBar } from './ProgressBar';
 export { ProgressIndicator } from './ProgressIndicator';
