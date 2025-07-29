75ad166c6733eb3ee05df8912bdc6fbd29376e68
bot19 <jsbot@pm.me>
2025-07-29 12:27:48 +1000

    step 3 WIP

--- src/components/onboardForm/StepCreatePassword.tsx
+++ src/components/onboardForm/StepCreatePassword.tsx
@@ -1,16 +1,27 @@
 import { EyeIcon } from '@heroicons/react/16/solid';
-import { Feedback, ProgressBar, ProgressIndicator } from '../../ui';
+import {
+  Button,
+  Feedback,
+  Heading,
+  InputBasic,
+  PasswordStrengthIndicator,
+  ProgressIndicator,
+} from '../../ui';
 import { useOnboarding } from '../../hooks/useOnboarding';
+import { useOnboardForm } from '../../hooks/useOnboardForm';
 
 export const StepCreatePassword = () => {
   const { currentStep, prevStep, nextStep } = useOnboarding();
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useOnboardForm();
 
   return (
     <div className="mt-10 mb-16 min-w-[320px] xs:mx-auto xs:w-full xs:max-w-[480px]">
       <div className="bg-white px-6 py-12 shadow-sm xs:rounded-lg xs:px-12">
-        <h2 className="mb-4 text-center text-2xl/9 font-bold tracking-tight text-secondary">
-          Sign in to your account
-        </h2>
+        <Heading text="Create your password" classes="mb-4" />
 
         <div className="mb-8 flex justify-center">
           <ProgressIndicator step={currentStep} />
@@ -19,87 +30,65 @@ export const StepCreatePassword = () => {
         <div className="space-y-6">
           <Feedback />
 
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Create your password
-            </label>
-            <div className="mt-2 flex">
-              <div className="-mr-px flex-1">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-l-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6"
-                />
-              </div>
-              <button
-                type="button"
-                className="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-primary cursor-pointer"
-              >
-                <EyeIcon
-                  aria-hidden="true"
-                  className="-ml-0.5 size-4 text-gray-400"
-                />
-                Show
-              </button>
-            </div>
-          </div>
+          <InputBasic
+            autoComplete="new-password"
+            label="Create your password"
+            requiredLabel
+            placeholder="Enter password"
+            type="password"
+            error={errors.password}
+            button="Show"
+            buttonIcon={
+              <EyeIcon
+                aria-hidden="true"
+                className="-ml-0.5 size-4 text-gray-400"
+              />
+            }
+            {...register('password')}
+          />
 
-          <ProgressBar />
+          <PasswordStrengthIndicator />
 
-          <div>
-            <label
-              htmlFor="confirm-password"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Confirm your password
-            </label>
-            <div className="mt-2 flex">
-              <div className="-mr-px flex-1">
-                <input
-                  id="confirm-password"
-                  name="confirm-password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-l-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6"
-                />
-              </div>
-              <button
-                type="button"
-                className="flex shrink-0 items-center gap-x-1.5 rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 outline-1 -outline-offset-1 outline-gray-300 hover:bg-gray-50 focus:relative focus:outline-2 focus:-outline-offset-2 focus:outline-primary cursor-pointer"
-              >
-                <EyeIcon
-                  aria-hidden="true"
-                  className="-ml-0.5 size-4 text-gray-400"
-                />
-                Show
-              </button>
-            </div>
-          </div>
+          <InputBasic
+            autoComplete="confirm-new-password"
+            label="Confirm your password"
+            requiredLabel
+            placeholder="Confirm password"
+            type="password"
+            error={errors.confirmPassword}
+            button="Show"
+            buttonIcon={
+              <EyeIcon
+                aria-hidden="true"
+                className="-ml-0.5 size-4 text-gray-400"
+              />
+            }
+            {...register('confirmPassword')}
+          />
 
           <div className="grid grid-cols-4 gap-4">
-            <button
+            <Button
+              colour="light"
+              text="Back"
               type="button"
               onClick={prevStep}
-              className="flex justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary cursor-pointer"
-            >
-              Back
-            </button>
+            />
 
             <div className="col-span-3">
-              <button
-                type="submit"
-                onClick={nextStep}
-                className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary cursor-pointer"
-              >
-                Submit
-              </button>
+              <Button
+                colour="primary"
+                text="Submit"
+                type="button"
+                onClick={() => {
+                  void trigger(['password', 'confirmPassword']).then(
+                    isValid => {
+                      if (isValid) {
+                        nextStep();
+                      }
+                    }
+                  );
+                }}
+              />
             </div>
           </div>
         </div>
